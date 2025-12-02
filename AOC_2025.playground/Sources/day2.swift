431c45881cdca2c70fcdc43b71cb393781c3c854
Björn Ro. <75440863+bstoss@users.noreply.github.com>
2025-12-02 21:09:57 +0100

    move code

--- AOC_2025.playground/Sources/day2.swift
+++ AOC_2025.playground/Sources/day2.swift
@@ -1,1 +1,40 @@
+import Foundation
 
+public func day2() {
+
+    do {
+        let fileUrl = Bundle.main.url(forResource: "day_2_source", withExtension: nil)!
+        let input = try String(contentsOf: fileUrl, encoding: .utf8).replacingOccurrences(of: "\n", with: "")
+        
+        let idRanges = input.split(separator: ",")
+        
+        var result = 0
+        for idRange in idRanges {
+            
+            let ids = idRange.split(separator: "-").map { Int($0)! }
+            
+            for id in ids[0]...ids[1] {
+                var stringId = "\(id)"
+                
+                var current = ""
+                
+                for char in stringId {
+                    
+                    current += String(char)
+                    
+                    guard current.count <= stringId.count/2 else { break }
+                    
+                    if stringId.replacingOccurrences(of: current, with: "").count == 0
+                    && current.count <= stringId.count/2 {
+                        result += id
+                        break
+                    }
+                }
+            }
+        }
+            
+        print(result)
+    } catch {
+        print(error)
+    }
+}
