5b746292a694297c9a1e58f2f3046229d72d79b1
Björn Ro. <75440863+bstoss@users.noreply.github.com>
2025-12-08 22:40:18 +0100

    day 8 still not done ... getting nuts

--- AOC_2025.playground/Contents.swift
+++ AOC_2025.playground/Contents.swift
@@ -1,33 +1,38 @@
 import Foundation
 
+let fileUrl = Bundle.main.url(forResource: "day_8_test", withExtension: nil)!
+let input = try! String(contentsOf: fileUrl, encoding: .utf8)
 
+let lines = input.split(separator: "\n")
+
+func debug(_ message: Any) {
+    print(message)
+}
 //distance between 2 objects in 3d space
 // d = √((x₂ - x₁)² + (y₂ - y₁)² + (z₂ - z₁)²)
 
-struct JunctionBox {
-    let x: Float
-    let y: Float
-    let z: Float
+struct JunctionBox: Hashable {
+    let x: Int
+    let y: Int
+    let z: Int
     
     init(_ input: String) {
-        var coords = input.split(separator: ",")
-        x = Float(coords[0])!
-        y = Float(coords[1])!
-        z = Float(coords[2])!
+        let coords = input.split(separator: ",")
+        x = Int(coords[0])!
+        y = Int(coords[1])!
+        z = Int(coords[2])!
     }
     
     var description: String {
         "\(x),\(y),\(z)"
     }
     
-    func distance(to box: JunctionBox) -> Double {
+    func distance(to box: JunctionBox) -> Int {
         let dx = box.x - x
         let dy = box.y - y
         let dz = box.z - z
-        return sqrt(Double(pow(dx, 2) + pow(dy, 2) + pow(dz, 2)))
+        return dx*dx + dy*dy + dz*dz
     }
-    
-    
 }
 
 extension JunctionBox: Equatable {
@@ -36,42 +41,195 @@ extension JunctionBox: Equatable {
     }
 }
 
-let input = """
-162,817,812
-57,618,57
-906,360,560
-592,479,940
-352,342,300
-466,668,158
-542,29,236
-431,825,988
-739,650,466
-52,470,668
-216,146,977
-819,987,18
-117,168,530
-805,96,715
-346,949,466
-970,615,88
-941,993,340
-862,61,35
-984,92,344
-425,690,689
-"""
-
-var lines = input.split(separator: "\n")
-var coords = lines.map { JunctionBox(String($0)) }
-func debug(_ message: Any) {
-    print(message)
+struct Circuit {
+    var boxes: Set<JunctionBox>
 }
 
-for coord in coords {
-    debug("CHECK \(coord.description)")
-    for coord2 in coords {
-        guard coord != coord2 else {
-            continue
-        }
-        debug("\(coord2.description)")
-        debug(coord.distance(to: coord2))
+struct Pair: Hashable {
+    let boxA: Int
+    let boxB: Int
+    let distance: Int
+}
+
+var junctionBoxes: [(x: Int, y: Int, z: Int)] = lines.map {
+    var coords = $0.split(separator: ",")
+    return (x: Int(coords[0])!, y: Int(coords[1])!, z: Int(coords[2])!)
+}
+var countBoxes = junctionBoxes.count
+var pairs: Set<Pair> = []
+pairs.reserveCapacity(countBoxes * (countBoxes - 1) / 2)
+
+print(Date())
+
+for i in 0..<junctionBoxes.count {
+    let (xi, yi, zi) = junctionBoxes[i]
+    for j in (i+1)..<junctionBoxes.count {
+        let (xj, yj, zj) = junctionBoxes[j]
+
+        let dx = xi - xj
+        let dy = yi - yj
+        let dz = zi - zj
+        let d = dx*dx + dy*dy + dz*dz
+        pairs.insert(
+            Pair(
+                boxA: i,
+                boxB: j,
+                distance: d
+            )
+        )
     }
 }
+
+print("PARIS")
+print(Date())
+
+let sortedPairs = pairs.sorted(by: { $0.distance < $1.distance })
+pairs = []
+print("SORTED")
+print(Date())
+
+var circuits: [Set<Int>] = []
+var merges = 0
+
+for pair in sortedPairs {
+    guard merges < 1000 else {
+        break
+    }
+    print("PAIR")
+    print("\(junctionBoxes[pair.boxA]) - \(junctionBoxes[pair.boxB]) - \(pair.distance)")
+    
+    if circuits.contains(where: { $0.contains(pair.boxA) && $0.contains(pair.boxB) })  {
+        continue
+    }
+    
+    if let firstIndex = circuits.firstIndex(where: { $0.contains(pair.boxA) || $0.contains(pair.boxB) }) {
+        circuits[firstIndex].insert(pair.boxA)
+        circuits[firstIndex].insert(pair.boxB)
+    } else {
+        circuits.append([pair.boxA])
+    }
+    
+    merges += 1
+}
+
+print(Date())
+print("DONE")
+
+ 
+let sortedCircuits = circuits.sorted(by: { $0.count > $1.count })
+
+sortedCircuits.forEach { circuit in
+    print("CIRCUIT: \(circuit.count)")
+}
+
+let amountOfWires = sortedCircuits[0..<3].reduce(into: 1) { partialResult, circuit in
+    print(circuit.count)
+    partialResult *= circuit.count
+}
+
+print(amountOfWires)
+
+//  4320 -> to low
+/*
+var circuits: [Circuit] = []
+var junctionBoxes = lines.map { JunctionBox(String($0)) }
+var toCheckAgainstBoxes = junctionBoxes.dropFirst()
+
+for _ in 0...10 {
+    var boxA = junctionBoxes[0]
+    var boxB = toCheckAgainstBoxes[1]
+    
+    var shortest: Double = boxA.distance(to: boxB)
+    for box in junctionBoxes {
+        
+        for checkAgainstBox in toCheckAgainstBoxes {
+            guard box != checkAgainstBox else {
+                continue
+            }
+            
+            let distance = box.distance(to: checkAgainstBox)
+            if distance < shortest {
+                boxA = box
+                boxB = checkAgainstBox
+                shortest = distance
+            }
+        }
+    }
+    print("\(boxA) - \(boxB) - \(shortest)")
+    toCheckAgainstBoxes.removeAll(where: { $0 == boxA || $0 == boxB })
+    
+    if let firstIndex = circuits.firstIndex(where: { $0.boxes.contains(boxA) || $0.boxes.contains(boxB) }) {
+        circuits[firstIndex].boxes.insert(boxA)
+        circuits[firstIndex].boxes.insert(boxB)
+    } else {
+        circuits.append(Circuit(boxes: [boxA, boxB]))
+    }
+}
+
+circuits.sort(by: { $0.boxes.count > $1.boxes.count })
+
+
+circuits.forEach { circuit in
+    print("CIRCUIT: \(circuit.boxes.count)")
+    circuit.boxes.forEach { box in
+        print(box)
+    }
+}
+
+
+let amountOfWires = circuits[0...3].reduce(into: 1) { partialResult, circuit in
+    print(circuit.boxes.count)
+    partialResult *= circuit.boxes.count
+}
+
+print(amountOfWires)
+*/
+/*
+for box in junctionBoxes {
+    
+    let nearestBox = junctionBoxes.filter({ $0 != box }).sorted(
+        by: {
+            box.distance(to: $0) < box.distance(to: $1)
+        }).first!
+    
+    print("\(box) -> \(nearestBox)")
+}
+*/
+/*
+ gehe jede junkbox durch
+ nehm nun stetig die beiden nähesten und pack die zusammen
+ 
+ */
+
+//var circuits: [Circuit] = []
+//
+//for coord in coords {
+//    debug("CHECK \(coord.description)")
+//    var shortest: JunctionBox?
+//    for coord2 in coords {
+//        guard coord != coord2 else {
+//            continue
+//        }
+//        
+//        guard let currentShortest = shortest else {
+//            shortest = coord2
+//            continue
+//        }
+//        
+//        debug("\(coord2.description)")
+//        let distance = coord.distance(to: coord2)
+//        let distanceToShortest = coord.distance(to: currentShortest)
+//        
+//        debug(distance)
+//        
+//        if distance < distanceToShortest {
+//            debug("SET NEW SHORTEST")
+//            shortest = coord2
+//        }
+//    }
+//    
+//    var circuit = Circuit(a: coord, b: shortest)
+//    circuits.append(circuit)
+//}
+//
+//print(circuits)
